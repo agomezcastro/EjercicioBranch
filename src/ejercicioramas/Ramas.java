3de214fd81010049f140538d2d9e3b265332b1f8
agomezcastro <agomezcastro@atanasoff04.danielcastelao.org>
2016-02-22 09:21:21 +0100

    Commit Bug

--- src/ejercicioramas/Ramas.java
+++ src/ejercicioramas/Ramas.java
@@ -1,9 +1,20 @@
 package ejercicioramas;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author agomezcastro
  */
 public class Ramas {
     // Ejercicio sobre las ramas
+    private int num1, num2, res;
+    
+    public int resta(){             
+        res =  num1 - num2;
+        num1 = Integer.parseInt(JOptionPane.showInputDialog("Introduce el 1er numero"));
+        num2 = Integer.parseInt(JOptionPane.showInputDialog("Introduce el 2º numero"));
+        System.out.println("Resultado= "+ res);
+        return res;
+    }
 }
